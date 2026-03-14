9cd21a628c0d95a8e52eb7b1e07913b9e0b51048
Sanvibongale0028 <sanvi.bongale@walchandsangli.ac.in>
2026-03-14 15:47:24 +0530

    Modified the Jenkinsfile and jenkinsService in backend

--- backend/services/jenkinsService.js
+++ backend/services/jenkinsService.js
@@ -1,3 +1,49 @@
+// const axios = require("axios");
+
+// const JENKINS_URL = process.env.JENKINS_URL;
+// const USERNAME = process.env.JENKINS_USERNAME;
+// const API_TOKEN = process.env.JENKINS_API_TOKEN;
+
+// async function triggerJenkinsJob(filePath) {
+
+//   try {
+
+//     const response = await axios.post(
+//       JENKINS_URL,
+//       new URLSearchParams({
+//         FILE_PATH: filePath
+//       }).toString(),
+//       {
+//         auth: {
+//           username: USERNAME,
+//           password: API_TOKEN
+//         },
+//         headers: {
+//           "Content-Type": "application/x-www-form-urlencoded"
+//         }
+//       }
+//     );
+
+//     console.log("Jenkins pipeline triggered successfully");
+
+//     return true;
+
+//   } catch (error) {
+
+//     console.error("Failed to trigger Jenkins pipeline");
+
+//     if (error.response) {
+//       console.error("Jenkins response:", error.response.data);
+//     } else {
+//       console.error("Error:", error.message);
+//     }
+
+//     return false;
+//   }
+// }
+
+// module.exports = { triggerJenkinsJob };
+
 const axios = require("axios");
 
 const JENKINS_URL = process.env.JENKINS_URL;
@@ -8,11 +54,13 @@ async function triggerJenkinsJob(filePath) {
 
   try {
 
-    const response = await axios.post(
+    const fullPath = `backend/${filePath}`;
+
+    await axios.post(
       JENKINS_URL,
       new URLSearchParams({
-        FILE_PATH: filePath
-      }).toString(),
+        FILE_PATH: fullPath
+      }),
       {
         auth: {
           username: USERNAME,
@@ -24,21 +72,12 @@ async function triggerJenkinsJob(filePath) {
       }
     );
 
-    console.log("Jenkins pipeline triggered successfully");
-
-    return true;
+    console.log("Jenkins pipeline triggered");
 
   } catch (error) {
 
-    console.error("Failed to trigger Jenkins pipeline");
+    console.error("Failed to trigger Jenkins:", error.message);
 
-    if (error.response) {
-      console.error("Jenkins response:", error.response.data);
-    } else {
-      console.error("Error:", error.message);
-    }
-
-    return false;
   }
 }
 
